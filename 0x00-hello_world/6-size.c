d1f0c386bcf0438869101ce72245f69df3c18e7d
Dave Omage <davillamags@gmail.com>
2023-07-14 11:34:42 -0700

    4-puts.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	float f;
+	char c;
+	printf("Size of a char: %lu \n"(unsigned long) sizeof(c));
+	printf("Size of an int: %lu \n"(unsigned long) sizeof(i));
+	printf("Size of a long int: %lu \n"(unsigned long) sizeof(%li));
+	printf("Size of a long long int: %lu \n"(unsigned long) sizeof(%llu));
+	printf("Size of a float: %lu \n"(unsigned long) sizeof(%f));
+}
